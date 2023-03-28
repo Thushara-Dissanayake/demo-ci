27fa0352d9280e8a308e48bdd41708fccc9a6765
thush1990 <thush1990@gmail.com>
2023-03-28 04:11:15 +0200

    commit

--- app/run.py
+++ app/run.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def home():
    return render_template('home.html')
+
+@app.route('/backend')
+def get_data():
+    return requests.get('http://backend:5200/msg/').content

9ae4074a6ce933d191a23a4d46c07dfbcf13745a
thush1990 <thush1990@gmail.com>
2023-03-28 04:27:56 +0200

    commit

--- app/run.py
+++ app/run.py
@@ -9,4 +9,4 @@ def home():
 
 @app.route('/backend')
 def get_data():
-    return requests.get('http://backend:5200/msg/').content
+    return requests.get('http://backend:5200/msg', headers={"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}).content

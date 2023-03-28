58e604994b40c13043cff885260079ea9f17309b
thush1990 <thush1990@gmail.com>
2023-03-28 10:54:11 +0200

    commit

--- app/run.py
+++ app/run.py
@@ -1,12 +1,17 @@
+# from flask import Flask
+# from flask_cors import CORS
+
+# app = Flask(__name__, static_url_path='', static_folder='static')
+# CORS(app)
+
+# @app.route('/msg/')
+# def home():
+#    return {'msg': 'This is a message from flask backend.'}
+
 from flask import Flask, render_template
-import requests
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
-@app.route('/')
+@app.route('/msg/')
 def home():
    return render_template('home.html')
-
-@app.route('/backend')
-def get_data():
-    return requests.get('http://backend:5200/msg/', headers={"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}).content
--- frontend/app/run.py
+++ frontend/app/run.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def home():
    return render_template('home.html')
+
+@app.route('/backend')
+def get_data():
+    return requests.get('http://backend:5200/msg/', headers={"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}).content

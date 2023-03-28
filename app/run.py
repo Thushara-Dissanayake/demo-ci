9e699be4c0962bb5252c3a1867eccd418f7713f8
thush1990 <thush1990@gmail.com>
2023-03-28 04:01:12 +0200

    commit

--- app/run.py
+++ app/run.py
@@ -1,17 +1,7 @@
-# from flask import Flask
-# from flask_cors import CORS
-
-# app = Flask(__name__, static_url_path='', static_folder='static')
-# CORS(app)
-
-# @app.route('/msg/')
-# def home():
-#    return {'msg': 'This is a message from flask backend.'}
-
 from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
-@app.route('/msg/')
+@app.route('/')
 def home():
    return render_template('home.html')
